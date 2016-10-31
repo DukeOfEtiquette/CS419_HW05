aedd146922c4945fa6b639358037959e9aabcb55
Adam DuQuette <duquetteadam@gmail.com>
2016-10-31 13:45:00 -0700

    Added signal handler and usage check for baseStation code

--- baseStation.py
+++ baseStation.py
@@ -1,4 +1,20 @@
+# !/usr/bin/python
+import sys
 import serial
+import socket
+import signal
 
-print("hello world")
+# Make sure usage is correct
+if (len(sys.argv) != 4):
+    print("usage: ./baseStation.py /dev/tty.usbXXX SENSOR SAMP_RATE")
+    print("SENSOR = 'ACC' || 'FLEX'")
+    print("SAMP_RATE = '5' || '50'")
+    sys.exit(1)
 
+
+# Register signal and define signal function
+def signal_handler(signal, frame):
+    print('\nExiting with the highest amount of grace')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
