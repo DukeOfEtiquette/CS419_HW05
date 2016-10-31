a34a84d99b8ed2242e9250d6b15f3296446aac94
Graham <ghough85@gmail.com>
2016-10-31 16:33:58 -0700

    Updates to flex and .ino file

--- hw05/hw05.ino
+++ hw05/hw05.ino
@@ -1,4 +1,6 @@
 #include <SFE_MMA8452Q.h>
+#include <Wire.h>
+#include <SFE_MMA8452Q.h>
 #include <MsTimer2.h>
 
 #define FLEX_PIN A0
@@ -10,51 +12,46 @@
 bool sensor = true; //ACC == true, FLX == false
 bool sample = true; //50hz == true, 5hz == false
 
+MMA8452Q accel;
+
 //TODO(adam): Update to not be based off the rheostat
 double getSamplingTime()
 {
-  int hz;
+  double hz;
   if(sample)
     hz = 50;
   else
     hz = 5;
-  //Interpolate rheostate value to desired Hz value
-  double hz = ( ( (MAX_HZ - MIN_HZ) /1023) * hz ) + MIN_HZ;
-
+    
   //Convert frequency to milliseconds and return value
   return 1000 / hz;
 }
 
 void sampleAcc()
 {
+  accel.read();
+  
+  
 }
 
 void sampleFlex()
 {
   int flexADC = analogRead(FLEX_PIN);
-  float flexV = flexADC * VCC / 1023.0
+  float flexV = flexADC * VCC / 1023.0;
   float flexR = R_DIV * (VCC / flexV - 1.0);
-  Serial.println("Resistance: " + String(FlexR) + " ohms");
+  Serial.println("Resistance: " + String(flexR) + " ohms");
 
   float angle = map(flexR, STRAIGHT_RES,  BEND_RES, 0, 90.0);
   Serial.println("Bend: " + String(angle) + " degrees");
   Serial.println();
-  
 }
 
 void startSampling()
 {
   if(sensor)
-     if(sample)
-        MsTimer2::set(getSamplingTime(), sampleACC);
-     else
-        MsTimer2::set(getSamplingTime(), sampleACC);
-  else if(!sensor)
-     if(sample)
-        MsTimer2::set(getSamplingTime(), sampleFlex);
-     else
-        MsTimer2::set(getSamplingTime(), sampleFlex);
-
+    MsTimer2::set(getSamplingTime(), sampleAcc);
+  else 
+     MsTimer2::set(getSamplingTime(), sampleFlex);
 
   MsTimer2::start();
 }
@@ -72,14 +69,14 @@ void getSamplingInfo()
   delay(100);
 
   //F is for FLEX
-  if(arg == F)
+  if(arg == 'F')
     sensor = false;
 
   arg = Serial.read();
   delay(100); //Give the serial port some time to catch up
 
   //S is for slow (5hz)
-  if(arg == S)
+  if(arg == 'S')
     sample = false;
 
   startSampling();
@@ -89,8 +86,7 @@ void setup() {
   // put your setup code here, to run once
   Serial.begin(9600);
   pinMode(FLEX_PIN, INPUT);
-  
-
+  accel.init();
 }
 
 void loop() {
@@ -104,9 +100,9 @@ void loop() {
 
   //N == new sampling request
   //T == stop current sampling
-  if(arg == N)
+  if(arg == 'N')
      getSamplingInfo();
-  else if(arg == T)
+  else if(arg == 'T')
      stopSampling();
 
 }
