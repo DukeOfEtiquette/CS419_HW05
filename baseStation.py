4a19b45f299cdfaf70704e2bc63b6d31978be3f5
Adam DuQuette <duquetteadam@gmail.com>
2016-10-31 13:55:10 -0700

    Verified usage works correctly

--- baseStation.py
+++ baseStation.py
@@ -11,6 +11,14 @@ if (len(sys.argv) != 4):
     print("SAMP_RATE = '5' || '50'")
     sys.exit(1)
 
+port = sys.argv[1]
+sensor = sys.argv[2]
+rate = sys.argv[3]
+
+print("Port: ", port)
+print("Sensor: ", sensor)
+print("Rate: ", rate)
+
 
 # Register signal and define signal function
 def signal_handler(signal, frame):
