f3663ceef755962021954b1784b8787f2e33de90
Graham <ghough85@gmail.com>
2016-10-31 19:19:25 -0700

    base2.py now has blank line repeating

--- base2.py
+++ base2.py
@@ -1,5 +1,22 @@
-# !/usr/bin/python
-#import serial
+#!/usr/bin/python
 
-arduino = serial.Serial('/dev/ttyACM0', 9600)
+import serial
+import time
+import sys
 
+arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
+time.sleep(2)
+
+arduino.flush()
+arduino.write('N')
+
+time.sleep(.1)
+
+arduino.flush()
+arduino.write('F')
+
+while True:
+	msg = arduino.read(arduino.inWaiting())
+	time.sleep(1)
+	sys.stdout.flush()
+	print(msg)
